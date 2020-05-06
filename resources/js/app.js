da1b7a43c10ea25435fc6dd7fe0d735971cc08eb
Toinetch <antoine.gouders@hotmail.com>
2020-05-06 13:34:24 +0200

    Intégration home

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+require('./jquery-2.1.4.min.js');
+global.$ = global.jQuery = require('jquery');
+require('./bootstrap.min.js');
+require('./magnific-popup.min.js');
+require('./owl.carousel.min.js');
+require('./circle-progress.min.js');
+require('./main.js');
---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.app')
+
+@section('content')
+
+    @include('templates.home.nav')
+
+    @include('templates.home.header')
+
+    @include('templates.home.about')
+
+    @include('templates.home.testimonials')
+
+    @include('templates.home.services')
+
+    @include('templates.home.team')
+
+    @include('templates.home.contact')
+
+    @include('templates.home.footer')
+
+@endsection
